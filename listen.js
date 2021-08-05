1507a3515e813d538d1fb1b9d98db807754ee8ba
mr-joelM <meyerjoel01@gmail.com>
2021-08-05 14:12:50 +0100

    listen set up

--- listen.js
+++ listen.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, ()=>{
-    console.log(`listening on PORT ${PORT}`)
-})
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
+  }
+);
